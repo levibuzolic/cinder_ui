b84857930a8a7cd4f06978e4fa9c067511addf65
levibuzolic <levibuzolic@gmail.com>
2026-06-19 15:44:14 +1000

    Bump version to 0.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,7 +2,7 @@
 
 All notable changes to this project will be documented in this file.
 
-## [0.1.0-beta.9] - 2026-06-19
+## [0.1.0] - 2026-06-19
 
 ### Added
 
--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule CinderUI.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/levibuzolic/cinder_ui"
-  @version "0.1.0-beta.9"
+  @version "0.1.0"
 
   def project do
     [
